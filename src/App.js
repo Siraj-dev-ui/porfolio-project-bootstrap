005ff2a466bcad54519c264f30df32fd863fed49
Siraj-dev-ui <sirajalig86@gmail.com>
2021-11-13 18:24:55 +0500

    navbar Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     
-      <h2>this is my is this</h2>
+      <Navbar/>
   );
 }
 
